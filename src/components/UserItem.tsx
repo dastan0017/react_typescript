80125953ce352a1a22063121bd42c8a861fe3702
d.rakhmanzhanov <dastan.rakhmanzhanov@timelysoft.net>
2022-12-03 13:42:02 +0300

    Component UserItem. prop 'key' should be passed from parent component NOT in Child component

---
+++ src/components/UserItem.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+import { IUser } from '../types/types'
+
+interface UserItemProps {
+    user: IUser
+}
+
+const UserItem: FC<UserItemProps> = ({ user }) => {
+    return (
+        <div style={{ padding: 15, border: "1px solid green" }}>
+            #{user.id}| {user.name} lives in city {user.address.city} on street {user.address.street}
+        </div>
+    )
+}
+
+export default UserItem
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { IUser } from '../types/types';
+import UserItem from './UserItem';
 
 interface UserListProps {
     users: IUser[]
@@ -9,9 +10,7 @@ const UserList: FC<UserListProps> = ({ users }) => {
     return (
         <div>
             {users.map(user =>
-                <div key={user.id} style={{ padding: 15, border: "1px solid green" }}>
-                    #{user.id}| {user.name} lives in city {user.address.city} on street {user.address.street}
-                </div>
+                <UserItem user={user} key={user.id} />
             )}
         </div>
     )
