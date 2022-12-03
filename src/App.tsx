46b5350c588e6d0780aa710be61c468b267ba71b
d.rakhmanzhanov <dastan.rakhmanzhanov@timelysoft.net>
2022-12-03 13:48:28 +0300

    Typing [axios request, useState hook]

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
 import UserList from './components/UserList';
 import { IUser } from './types/types';
 
 function App() {
-  const users: IUser[] = [
-    { id: 1, name: "Dastan", address: { city: "Bishkek", zipcode: "123456", street: "Baba-Ata" }, email: "asdf@gmail.com" },
-    { id: 2, name: "Dastan", address: { city: "Bishkek", zipcode: "123456", street: "Baba-Ata" }, email: "asdf@gmail.com" }
-  ]
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users")
+      setUsers(response.data)
+    } catch (err) {
+      alert(err)
+    }
+  }
 
   return (
     <div className="App">
