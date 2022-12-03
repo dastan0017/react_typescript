0400ccee3e9f20938e30c8cdfb02bf5f0be1be76
d.rakhmanzhanov <dastan.rakhmanzhanov@timelysoft.net>
2022-12-03 15:45:47 +0300

    Typing events: MouseEvents, DragEvents, ChangeEvents

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
+import EventsExample from './components/EventsExample';
 import List from './components/List';
 import TodoItem from './components/TodoItem';
 import UserItem from './components/UserItem';
@@ -37,6 +38,7 @@ function App() {
   return (
     <div className="App">
       <h1>Working!</h1>
+      <EventsExample />
       <Card width='200px' height='200px' variant={CardVariant.outlined} onClick={() => console.log("Click")}>
         <button>Click me pls</button>
       </Card>
---
+++ src/components/EventsExample.tsx
@@ -0,0 +1,49 @@
+import React, { FC, useState } from 'react'
+
+const EventsExample: FC = () => {
+    const [value, setValue] = useState<string>("")
+    const [isDrag, setIsDrag] = useState<boolean>(false);
+
+    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value)
+    }
+
+    const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log(value)
+    }
+
+    const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        console.log("DRAGGING")
+    }
+
+    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(true);
+    }
+
+    const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(false);
+    }
+
+    const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(true);
+        console.log("Dropped")
+    }
+
+    return (
+        <div>
+            <input type="text" value={value} onChange={changeHandler} />
+            <button onClick={clickHandler}>BUTTON</button>
+            <div onDrag={dragHandler} draggable style={{ width: 200, height: 200, background: 'red' }}></div>
+            <div
+                onDrop={dropHandler}
+                onDragLeave={handleDragLeave}
+                onDragOver={handleDragOver}
+                style={{ width: 200, height: 200, background: isDrag ? 'blue' : 'red', marginTop: 15 }}></div>
+        </div>
+    )
+}
+
+export default EventsExample
