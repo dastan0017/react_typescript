4a2d586c19efa29a87bffb0c2d9e3f26e1dee8ee
d.rakhmanzhanov <dastan.rakhmanzhanov@timelysoft.net>
2022-12-03 13:18:48 +0300

    React.FC for typing Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Card from './components/Card';
+import Card, { CardVariant } from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <h1>Working!</h1>
-      <Card width='500px' height='500px' >
+      <Card width='500px' height='500px' variant={CardVariant.outlined} onClick={() => console.log("Click")}>
         <button>Click me pls</button>
       </Card>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
 
+export enum CardVariant {
+    outlined = "outlined",
+    primary = "primary"
+}
+
 interface CardProps {
     width: string;
     height: string;
-    children: React.ReactChild | React.ReactNode
+    children: React.ReactChild | React.ReactNode;
+    variant: CardVariant;
+    onClick: () => void
 }
 
-const Card = ({ width, height, children }: CardProps) => {
+const Card: React.FC<CardProps> = ({ width, height, children, variant, onClick }) => {
     return (
-        <div style={{ width, height, border: '2px solid gray' }}>
+        <div style={{
+            width,
+            height,
+            border: variant === CardVariant.outlined ? '2px solid gray' : "none",
+            background: variant === CardVariant.primary ? "lightgray" : ""
+        }}
+            onClick={onClick}
+        >
             {children}
         </div>
     )
