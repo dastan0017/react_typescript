57b3525be999c3897f3b9c65edb8211a01b4d69b
d.rakhmanzhanov <dastan.rakhmanzhanov@timelysoft.net>
2022-12-03 15:50:48 +0300

    Typing 'useRef' hook

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,15 +1,17 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useRef, useState } from 'react'
 
 const EventsExample: FC = () => {
     const [value, setValue] = useState<string>("")
     const [isDrag, setIsDrag] = useState<boolean>(false);
+
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
     }
 
     const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log(value)
+        console.log(inputRef.current?.value)
     }
 
     const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -34,14 +36,17 @@ const EventsExample: FC = () => {
 
     return (
         <div>
-            <input type="text" value={value} onChange={changeHandler} />
+            <input type="text" value={value} onChange={changeHandler} placeholder="Controllable" />
+            <input type="text" placeholder="Uncontrollable" ref={inputRef} />
+
             <button onClick={clickHandler}>BUTTON</button>
             <div onDrag={dragHandler} draggable style={{ width: 200, height: 200, background: 'red' }}></div>
             <div
                 onDrop={dropHandler}
                 onDragLeave={handleDragLeave}
                 onDragOver={handleDragOver}
-                style={{ width: 200, height: 200, background: isDrag ? 'blue' : 'red', marginTop: 15 }}></div>
+                style={{ width: 200, height: 200, background: isDrag ? 'blue' : 'red', marginTop: 15 }}>
+            </div>
         </div>
     )
 }
