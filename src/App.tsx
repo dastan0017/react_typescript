6535eda985d7d29090dba3e72cd5e562b470cd66
d.rakhmanzhanov <dastan.rakhmanzhanov@timelysoft.net>
2022-12-03 13:04:50 +0300

    Typing props. Interface CardProps. Typing 'children'.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <h1>Working!</h1>
+      <Card width='500px' height='500px' >
+        <button>Click me pls</button>
+      </Card>
     </div>
   );
 }
